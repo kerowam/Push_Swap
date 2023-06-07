00e7d6db77fddd1c3f23f68f23ffd544db181559
Germán Fredes <germanfredes1988@protonmail.com>
2023-06-07 13:17:26 +0200

    main() added to put arguments in stack_a.

--- push_swap.c
+++ push_swap.c
@@ -10,3 +10,25 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "push_swap.h"
+#include "libft/libft.h"
+
+int	main(int argc, char **argv) {
+	t_node	*stack_a;
+	t_node	*stack_b;
+
+	stack_a = NULL;
+	stack_b = NULL;
+	if (argc < 2) {
+		printf("Error: no arguments\n");
+		return 0;
+	}
+	int arg = 1;
+	while (arg < argc) {
+		insert_up(&stack_a, ft_atoi(argv[arg]));
+		arg++;
+	}
+	
+
+	return 0;
+}
