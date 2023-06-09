1e4b7fac33d59c824066d1a04f81c7b72399771e
Germán Fredes <germanfredes1988@protonmail.com>
2023-06-09 21:02:22 +0200

    quick_sort removed to use LIS algorithm

--- sorts.c
+++ sorts.c
@@ -12,22 +12,6 @@
 
 #include "push_swap.h"
 
-static int	get_pivot_value(t_node **stack, int size)
-{
-	int	value;
-	int	i;
-
-	i = 0;
-	while (i < size / 2)
-	{
-		value = (*stack)->value;
-		*stack = (*stack)->next;
-		i++;
-	}
-	return (value);
-}
-
-
 void sort_three(t_node **stack_a)
 {
 	if (size == 1)
@@ -50,41 +34,5 @@ void sort_three(t_node **stack_a)
 	}
 }
 
-void	quick_sort_a(t_node **stack_a, t_node **stack_b, int size)
-{
-	int	pivot_index;
-	int pivot_value;
-	int i;
 
-	if (size <= 3) {
-		sort_three(stack_a);
-		return;
-	}
-	else if (size > 3 && size < 100) {
-		pivot_index = size / 2;
-		pivot_value = get_pivot_value(stack_a, size);
-		i = 0;
-		while (i < pivot_index)
-		{
-			if ((*stack_a)->value < pivot_value) {
-				pb(stack_a, stack_b);
-				i++;
-			} else {
-				ra(stack_a);
-				i++;
-			}
-			stack_a = &(*stack_a)->next;
-		}
-		while (i < size) {
-			if ((*stack_a)->value < pivot_value) {
-				pb(stack_a, stack_b);
-				i++;
-			} else {
-				ra(stack_a);
-				i++;
-			}
-			stack_a = &(*stack_a)->next;
-		}
-		//quick_sort_a(stack_a, stack_b, pivot_index); ???????
-	}
-}
+
