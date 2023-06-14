dcdd52dfe4fc1b3edfe5bee508c3abd99a332e14
Germán Fredes <germanfredes1988@protonmail.com>
2023-06-14 22:34:07 +0200

    reverse_sort_three ft added

--- sorts.c
+++ sorts.c
@@ -12,7 +12,7 @@
 
 #include "push_swap.h"
 
-void sort_three(t_node **stack_a, size_t size)
+void	sort_three(t_node **stack_a, size_t size)
 {
 	if (size == 1)
 		return;
@@ -34,5 +34,24 @@ void sort_three(t_node **stack_a, size_t size)
 	}
 }
 
-
-
+void	reverse_sort_three(t_node **stack_a, size_t size)
+{
+	if (size == 1)
+		return;
+	else if (size == 2)
+	{
+		if ((*stack_a)->value < (*stack_a)->next->value)
+			sa(stack_a);
+		return;
+	}
+	else if (size == 3)
+	{
+		if ((*stack_a)->value < (*stack_a)->next->value)
+			sa(stack_a);
+		if ((*stack_a)->value > (*stack_a)->next->next->value)
+			rra(stack_a);
+		else if ((*stack_a)->value < (*stack_a)->next->next->value)
+			ra(stack_a);
+		return;
+	}
+}
